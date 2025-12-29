fa4f1534bba8b8937b17b8c343a87b81a4a55aa4
Akash <aakashkumarag96@gmail.com>
2025-12-29 11:26:49 +0530

    Solved a question based on lazy loading

--- lazy_loading/src/App.jsx
+++ lazy_loading/src/App.jsx
@@ -1,11 +1,25 @@
+import React, { useState } from "react";
 import "./App.css";
+import { Suspense } from "react";
+
+const Dashboard = React.lazy(() => import("./components/Dashboard"))
+const Profile = React.lazy(() => import ("./components/Profile"))
 
 function App() {
+  const [page, setPage] = useState("dashboard");
 
   return (
     <div className="app-shell">
       <div className="assignment-card">
-        
+        <h2>Welcome to Dashboard</h2>
+
+        <button onClick={() => setPage("dashboard")} >Dashboard</button>
+        <button onClick={() => setPage("profile")}>Profile</button>
+      
+        <Suspense fallback={<h2>Loading....</h2>}>
+          {page === "dashboard" && <Dashboard />}
+          {page === "profile" && <Profile />}
+        </Suspense>
       </div>
     </div>
   )
---
+++ lazy_loading/src/components/Profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Profile() {
+    console.log("Profile rendred");
+  return (
+    <div>
+        <h2>Profile</h2>
+    </div>
+  )
+}
